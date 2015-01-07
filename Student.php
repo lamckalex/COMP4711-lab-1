b6ed01f87fb84765fd7396d6206c7eedd9a7b1c5
a00880208 <alam160@my.bcit.ca>
2015-01-07 13:22:18 -0800

    Fixed average math

--- Student.php
+++ Student.php
@@ -32,8 +32,8 @@ class Student {
         foreach($this->grades as $value)
         {
             $total += $value;
-            return $total / count($this->grades);
         }
+        return round($total / count($this->grades));
     }
     
     function toString(){
